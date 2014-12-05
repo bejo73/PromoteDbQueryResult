9ce514dd864975bf4f0980f899472368642bd856
Jörgen Bergström <jorgen.bergstrom@ibiz-solutions.se>
2014-12-05 11:00:53 +0100

    Renamed project

--- Src/PromoteDbQueryResult/ContextPropertyValueExists/Properties/AssemblyInfo.cs
+++ Src/PromoteDbQueryResult/ContextPropertyValueExists/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("BizTalkComponents.PipelineComponents.PromoteDbQueryResult.ContextPropertyValueExists")]
+[assembly: AssemblyTitle("BizTalkComponents.PipelineComponents.PromoteDbQueryResult")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("BizTalkComponents.PipelineComponents.PromoteDbQueryResult.ContextPropertyValueExists")]
+[assembly: AssemblyProduct("BizTalkComponents.PipelineComponents.PromoteDbQueryResult")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
